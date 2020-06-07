4a43cf541eb749995a047f9a469d4c795afb0821
Prashant Pokhriyal <pokhariyalp@gmail.com>
2020-06-07 23:32:32 +0530

    update widgets component

--- components/widgets/widgets.module.js
+++ components/widgets/widgets.module.js
@@ -5,5 +5,7 @@
         .module('app.widgets', [
             'app.widgets.summary',
             'app.widgets.add',
+            'app.widgets.edit',
+            'app.widgets.detail',
         ]);
 })();
