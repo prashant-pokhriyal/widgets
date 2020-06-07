f02b40a0f9aee8855ed1008439c6b9e9468fc77e
Prashant Pokhriyal <pokhariyalp@gmail.com>
2020-06-07 23:46:02 +0530

    check if id exists in detail module

--- components/widgets/detail/detail.ctrl.js
+++ components/widgets/detail/detail.ctrl.js
@@ -5,12 +5,15 @@
         .module('app.widgets.detail')
         .controller('detailCtrl', detailCtrl);
 
-    detailCtrl.$inject = ['widgetsService', '$stateParams'];
+    detailCtrl.$inject = ['widgetsService', '$stateParams', '$state'];
 
-    function detailCtrl(widgetsService, $stateParams) {
+    function detailCtrl(widgetsService, $stateParams, $state) {
         const vm = this;
         vm.id = $stateParams.id;
         vm.data = widgetsService.get(vm.id);
+        if (vm.data === undefined) {
+            $state.go('widgets.summary');
+        }
         console.log(vm.data);
     }
 })();
